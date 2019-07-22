835b584d3dff2985fdd4de9f780ac34be4c80996
chenjiangchao <1455075085@qq.com>
2019-07-22 20:53:49 +0800

    解决冲突

--- src/com/github/domain/Text.java
+++ src/com/github/domain/Text.java
@@ -6,6 +6,8 @@
  */
 package com.github.domain;
 
+import java.util.Date;
+
 /**
  * 〈一句话功能简述〉<br>
  * 〈文章〉
@@ -15,4 +17,80 @@ package com.github.domain;
  * @since 1.0.0
  */
 public class Text {
+    private String userid;
+    private int textid;
+    private Date time;
+    private int likes;
+    private int comment;
+    private int collection;
+    private String text;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public int getTextid() {
+        return textid;
+    }
+
+    public void setTextid(int textid) {
+        this.textid = textid;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getComment() {
+        return comment;
+    }
+
+    public void setComment(int comment) {
+        this.comment = comment;
+    }
+
+    public int getCollection() {
+        return collection;
+    }
+
+    public void setCollection(int collection) {
+        this.collection = collection;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Text{" +
+                "userid='" + userid + '\'' +
+                ", textid=" + textid +
+                ", time=" + time +
+                ", likes=" + likes +
+                ", comment=" + comment +
+                ", collection=" + collection +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
--- src/com/github/service/TextService.java
+++ src/com/github/service/TextService.java
@@ -1,4 +1,7 @@
 package com.github.service;
 
+import com.github.domain.Text;
+
 public interface TextService {
+    void createText(Text text);
 }
