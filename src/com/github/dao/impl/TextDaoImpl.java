a7eae1bf495ed31230030e50e8037173a1d2ded7
chenjiangchao <1455075085@qq.com>
2019-07-22 19:48:33 +0800

    文章存储

---
+++ src/com/github/dao/impl/TextDaoImpl.java
@@ -0,0 +1,18 @@
+/**
+ * FileName: TextDaoImpl
+ * Author:   陈江超
+ * Date:     2019/7/22 11:35
+ * Description: TextDao实现
+ */
+package com.github.dao.impl;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈TextDao实现〉
+ *
+ * @author 陈江超
+ * @create 2019/7/22
+ * @since 1.0.0
+ */
+public class TextDaoImpl {
+}
