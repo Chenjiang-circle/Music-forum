da9ff4be29bdcdadd07d8b90c231d292076c1c46
InfinityOasis <2464792469.qq.com>
2019-07-23 19:30:44 +0800

    update

---
+++ out/artifacts/Music_forum_war_exploded/515BBS_v0.1-注册与登录/js/demo-search.js
@@ -0,0 +1,54 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2016, Codrops
+ * http://www.codrops.com
+ */
+;(function(window) {
+
+	'use strict';
+
+	var mainContainer = document.querySelector('.main-wrap'),//主页面
+		openCtrl = document.getElementById('codrops-header'),//搜索图标
+		closeCtrl = document.getElementById('btn-search-close'),//关闭图标
+		searchContainer = document.querySelector('.search'),//搜索内容
+		inputSearch = searchContainer.querySelector('.search__input');//搜索栏
+
+	function init() {
+		initEvents();	
+	}
+
+	function initEvents() {
+		openCtrl.addEventListener('click', openSearch);
+		closeCtrl.addEventListener('click', closeSearch);
+		document.addEventListener('keyup', function(ev) {
+			// escape key.
+			if( ev.keyCode == 27 ) {
+				closeSearch();
+			}
+		});
+	}
+
+	function openSearch() {
+		mainContainer.classList.add('main-wrap--move');
+		searchContainer.classList.add('search--open');
+		inputSearch.focus();
+		$('.main-wrap').css({'overflow':'hidden'})
+	}
+
+	function closeSearch() {
+		mainContainer.classList.remove('main-wrap--move');
+		searchContainer.classList.remove('search--open');
+		inputSearch.blur();
+		inputSearch.value = '';
+		$('#recommend').css({'top':'-20px','opacity':'0'});
+		$('.main-wrap').css({'overflow':'auto'})
+	}
+
+	init();
+
+})(window);
