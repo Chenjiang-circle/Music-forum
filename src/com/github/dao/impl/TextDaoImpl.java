765ce17b6ac9c4691563fd9527ce6433def25935
chenjiangchao <1455075085@qq.com>
2019-07-24 21:10:38 +0800

    实现将评论插入数据库

--- src/com/github/dao/impl/TextDaoImpl.java
+++ src/com/github/dao/impl/TextDaoImpl.java
@@ -8,7 +8,9 @@ package com.github.dao.impl;
 
 import com.github.dao.TextDao;
 import com.github.domain.Text;
+import com.github.domain.comment;
 import com.github.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -23,7 +25,39 @@ public class TextDaoImpl implements TextDao {
     private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
     @Override
     public void createText(Text text) {
-        String sql = "insert into text values(?, null, ?, ?, ?, ?, ?)";
-        template.update(sql, text.getUserid(), text.getTime(), text.getLikes(), text.getComment(), text.getCollection(), text.getText());
+        String sql = "insert into text values(?, null, ?, ?, ?, ?, ?, ?, ?)";
+        template.update(sql, text.getUserid(), text.getTime(), text.getLikes(), text.getComment(), text.getCollection(), text.getText(), text.getTitle(), text.getType());
     }
+
+    @Override
+    public Boolean addComment(comment comments, int commentid) {
+        try {
+            String sql = "insert into comment values(?, ?, ?, ?)";
+            template.update(sql, commentid, comments.getTextid(), comments.getText(), comments.getTime());
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("在插入comment表时，出现异常");
+            return false;
+        }
+    }
+
+    @Override
+    public int addCommentToText(comment comments) {
+        try {
+            // 先将评论存入text表中，生成一个唯一的textid
+            String sql = "insert into text values(?, null, ?, ?, ?, ?, ?, ?, ?)";
+            template.update(sql, comments.getUserid(), comments.getTime(), comments.getLikes(), comments.getComment(), comments.getCollection(), comments.getText(), comments.getTitle(), comments.getType());
+            // 获取这个唯一的textid，并返回值
+            String sql2 = "select * from text where text = ? and userid = ? ";
+            comment comment = template.queryForObject(sql2, new BeanPropertyRowMapper<comment>(comment.class), comments.getText(), comments.getUserid());
+            return comment.getTextid();
+        }catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("将评论插入文章表时出现异常,返回-1");
+            return -1;
+        }
+    }
+
+
 }
--- src/com/github/service/TextService.java
+++ src/com/github/service/TextService.java
@@ -1,7 +1,19 @@
 package com.github.service;
 
 import com.github.domain.Text;
+import com.github.domain.comment;
 
 public interface TextService {
+    /**
+     * 发表文章
+     * @param text
+     */
     void createText(Text text);
+
+    /**
+     * 添加评论
+     * @param comments
+     * @return
+     */
+    Boolean addComment(comment comments);
 }
